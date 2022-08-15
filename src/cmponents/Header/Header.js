2f2a612fb72984b95c8fcaeb15a907ff56b725ec
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-15 12:44:12 +0600

    added header and nav bar code

--- src/cmponents/Header/Header.js
+++ src/cmponents/Header/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
-import logo from '../../images/amazon.png'
+import logo from '../../images/amazon.png';
+import './Header.css';
+
 const Header = () => {
     return(
-        <div>
+        <div className="header">
             <img src={logo} alt="" />
+            <nav>
+                <a href="/">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
